1933c0694d4e76c51133938554c9aca409eb9cd0
JuanTG08 <64704012+JuanTG08@users.noreply.github.com>
2022-10-01 22:16:01 -0500

    :green_heart: Anexamos las variables de entorno

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,13 @@
 import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
-
+// Obtenemos las variables de configuraciones
+import env from './config/config';
 // Creamos la variable APP para el inicio del servidor
 const app = express();
 
 // Configuraciones previas
-app.set('port', 8000);
+app.set('port', env.PORT_SERVER);
 app.use(morgan('dev'));
 app.use(cors());
 
