b20ca0fe1efda518917e9c70164e9b1d2ab98462
JuanTG08 <64704012+JuanTG08@users.noreply.github.com>
2022-10-01 22:38:56 -0500

    :pushpin: Se agrego el archivo socket

---
+++ src/socket.io.ts
@@ -0,0 +1,14 @@
+// import TravelCtrl from "./controller/travel.controller";
+
+export default (io: any) => {
+    // Middlewares para socket
+    /*
+    io.use((socket: any, next: any) => {
+        console.log(socket);
+        next();
+    })
+    */
+
+    io.on('connection', (socket: any) => {
+    });
+}
