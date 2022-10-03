f7e50b01459c5493ca9b1e9182d965721164e77c
JuanTG08 <64704012+JuanTG08@users.noreply.github.com>
2022-10-03 15:12:00 -0500

    :seedling: Se creo el modelo de los files

---
+++ src/model/files.model.ts
@@ -0,0 +1,103 @@
+import Hook from "../config/utils";
+import Files from "./schema/files.schemas";
+
+class FilesModel {
+  static create(data: any) {
+    const schema = new Files(data);
+    return schema
+      .save()
+      .then((resp) => {
+        return Hook.Message(false, 200, "Se guardo el correctamente");
+      })
+      .catch((err) => {
+        console.log(err);
+        return Hook.Message(
+          false,
+          500,
+          "Error al intentar generar esta acción"
+        );
+      });
+  }
+
+  static findOneById(_id: string) {
+    return Files.find({
+      _id,
+    })
+      .then((resp) => {
+        if (resp.length > 0) return Hook.Message(false, 200, "Ok", resp);
+        return Hook.Message(false, 501, "No se encontro nada.");
+      })
+      .catch((err) => {
+        return Hook.Message(
+          false,
+          500,
+          "Error al intentar generar esta acción"
+        );
+      });
+  }
+
+  static findAny(query: any) {
+    return Files.find(query)
+      .then((resp) => {
+        if (resp.length > 0) return Hook.Message(false, 200, "Ok", resp);
+        return Hook.Message(false, 501, "No se encontro nada.");
+      })
+      .catch((err) => {
+        return Hook.Message(
+          false,
+          500,
+          "Error al intentar generar esta acción"
+        );
+      });
+  }
+
+  static modify(data: any, _id: string) {
+    return Files.findByIdAndUpdate(
+      { _id },
+      data
+    )
+      .then((resp) => {
+        return Hook.Message(false, 200, "Se Actualizo correctamente");
+      })
+      .catch((err) => {
+        return Hook.Message(
+          false,
+          500,
+          "Error al intentar generar esta acción"
+        );
+      });
+  }
+
+  static disable(_id: string) {
+    return Files.findByIdAndUpdate(
+      { _id },
+      { status: false }
+    )
+      .then((resp) => {
+        return Hook.Message(false, 200, "Se deshabilito correctamente");
+      })
+      .catch((err) => {
+        return Hook.Message(
+          false,
+          500,
+          "Error al intentar generar esta acción"
+        );
+      });
+  }
+
+  static delete(_id: string) {
+    return Files.findByIdAndRemove({ _id })
+      .then((resp) => {
+        return Hook.Message(false, 200, "Se Elimino correctamente");
+      })
+      .catch((err) => {
+        return Hook.Message(
+          false,
+          500,
+          "Error al intentar generar esta acción"
+        );
+      });
+  }
+}
+
+export default FilesModel;
