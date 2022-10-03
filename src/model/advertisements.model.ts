ad70a34461bc5143ee03ad24b01289000727cae7
JuanTG08 <64704012+JuanTG08@users.noreply.github.com>
2022-10-03 09:38:55 -0500

    :sparkles: Creamos el modelo para los anuncios

---
+++ src/model/advertisements.model.ts
@@ -0,0 +1,74 @@
+import { ObjectId } from "mongoose";
+import Hook from "../config/utils";
+import AdvertisementsPages from "./schema/advertisements.schema";
+
+class RolModel {
+    static create(data: any) {
+        const page = new AdvertisementsPages(data);
+        return page.save()
+            .then(resp => {
+                return Hook.Message(false, 200, "Se guardo el correctamente");
+            }).catch(err => {
+                console.log(err);
+                return Hook.Message(false, 500, "Error al intentar generar esta acción");
+            });
+    }
+    static listAll() {
+        return AdvertisementsPages.find()
+            .then(resp => {
+                if (resp.length > 0) return Hook.Message(false, 200, "Ok", resp);
+                return Hook.Message(false, 501, "No se encontro nada.");
+            }).catch(err => {
+                return Hook.Message(false, 500, "Error al intentar generar esta acción");
+            });
+    }
+
+    static findOneById(_id: ObjectId) {
+        return AdvertisementsPages.find({ _id })
+            .then(resp => {
+                if (resp.length > 0) return Hook.Message(false, 200, "Ok", resp);
+                return Hook.Message(false, 501, "No se encontro nada.");
+            }).catch(err => {
+                return Hook.Message(false, 500, "Error al intentar generar esta acción");
+            });
+    }
+
+    static findAny(query: any) {
+        return AdvertisementsPages.find(query)
+            .then(resp => {
+                if (resp.length > 0) return Hook.Message(false, 200, "Ok", resp);
+                return Hook.Message(false, 501, "No se encontro nada.");
+            }).catch(err => {
+                return Hook.Message(false, 500, "Error al intentar generar esta acción");
+            });
+    }
+
+    static modify(data: any, _id: ObjectId) {
+        return AdvertisementsPages.findByIdAndUpdate(_id, data)
+            .then(resp => {
+                return Hook.Message(false, 200, "Se Actualizo correctamente");
+            }).catch(err => {
+                return Hook.Message(false, 500, "Error al intentar generar esta acción");
+            });
+    }
+
+    static disable(_id: ObjectId) {
+        return AdvertisementsPages.findByIdAndUpdate(_id, { status: false })
+            .then(resp => {
+                return Hook.Message(false, 200, "Se deshabilito correctamente");
+            }).catch(err => {
+                return Hook.Message(false, 500, "Error al intentar generar esta acción");
+            });
+    }
+
+    static delete(_id: ObjectId) {
+        return AdvertisementsPages.findByIdAndRemove(_id)
+            .then(resp => {
+                return Hook.Message(false, 200, "Se Elimino correctamente");
+            }).catch(err => {
+                return Hook.Message(false, 500, "Error al intentar generar esta acción");
+            });
+    }
+}
+
+export default RolModel;
